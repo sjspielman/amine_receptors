cfc10f1f4f0c7532d3cb3c43df05a5a7971a1e67
sjspielman <stephanie.spielman@gmail.com>
2014-05-17 10:35:34 -0500

    bug fixes

--- scripts/hmm_scripts/confirmGPCR.py
+++ scripts/hmm_scripts/confirmGPCR.py
@@ -9,16 +9,19 @@ import re
 from Bio import SeqIO
 
 
-assert( len(sys.argv) == 3), "Usage: python confirmGPCR.py <infile> <outfile>. File should be in working directory. No paths needed."
+assert( len(sys.argv) == 4), "Usage: python confirmGPCR.py <infile> <outfile> <outfile_directory>. infile should be in working directory."
 
-gpcrhmm_path = '/home/sjs3495/bin/gpcrhmm/'
+gpcrhmm_path = 'gpcrhmm/'
 wdir = os.getcwd() + '/'
 infile = wdir + sys.argv[1] # file containing all sequences. should be fasta format
-outfile = wdir + sys.argv[2]
+outfile = sys.argv[2]
+rdir = sys.argv[3]
+outfile = rdir + '/' + outfile
+
 
 ###################################################################################
 def runGPCRHMM(gpcrhmm_path, infile, outfile):
-	command = 'perl '+gpcrhmm_path+'/gpcrhmm.pl '+infile+' > '+outfile
+	command = 'perl '+gpcrhmm_path+'gpcrhmm.pl '+infile+' > '+outfile
 	run = subprocess.call(command, shell=True)
 	assert(run == 0), "gpcrhmm did not properly run."
 
@@ -27,15 +30,23 @@ def parseGPCRHMM(seqid, outfile):
 	outf = open(outfile, 'rU')
 	line = outf.readlines()[1]
 	outf.close()
-	getScores = re.search('^'+seqid+'\s+(\d*\.*\d*)\s+(\d*\.*\d*)\s+\w+', line)
+	getScores = re.search('^'+seqid+'\s+(-*\d*\.*\d*)\s+(-*\d*\.*\d*)\s+(\w+)\s*', line)
 	assert(getScores), "Couldn't parse the gpcrhmm output file."
-	globalScore = float(getScores.group(1))
-	localScore = float(getScores.group(2))
-	return (globalScore, localScore)
+	globalScore = getScores.group(1)
+	localScore = getScores.group(2)
+	pred = getScores.group(3)
+	if pred == 'GPCR':
+		if globalScore == '-':
+			globalScore = 0.
+		if localScore == '-':
+			localScore = 0.
+		return(True, float(globalScore), float(localScore))
+	else:
+		return (False, globalScore, localScore)
 ###################################################################################
 
-globalThresh = 50
-localThresh = 50
+globalThresh = 10
+localThresh = 10
 
 gpcrhmmIn = wdir+'tempin.fasta'
 gpcrhmmOut = wdir+'tempout.txt'
@@ -52,12 +63,19 @@ for record in records:
 	temp.write(">"+id+"\n"+seq+"\n")
 	temp.close()
 	runGPCRHMM(gpcrhmm_path, gpcrhmmIn, gpcrhmmOut)
-	globalScore, localScore = parseGPCRHMM(id, gpcrhmmOut)
-	
-	if globalScore >= globalThresh and localScore >= localThresh:
-		outf.write(">"+id+"\n"+seq+"\n")
+	isGPCR, globalScore, localScore = parseGPCRHMM(id, gpcrhmmOut)
+	if isGPCR:
+		print "it's a gpcr with",globalScore, localScore
+		if globalScore >= globalThresh and localScore >= localThresh:
+			print "keep!"
+			outf.write(">"+id+"\n"+seq+"\n")
+		else:
+			print "but discard"
+			outculled.write(id+'\t'+str(globalScore)+'\t'+str(localScore)+'\n')
 	else:
+		print "not a gpcr at all"
 		outculled.write(id+'\t'+str(globalScore)+'\t'+str(localScore)+'\n')
+	print
 outf.close()
 
 
