3b0f0aa52baf43e59883c8ee623e671b7acdfaba
Stephanie J Spielman <stephanie.spielman@gmail.com>
2014-10-30 15:56:24 -0500

    process_blast now contains the gpcrhmm stuff again. consolidated and commented more

--- scripts/process_blast.py
+++ scripts/process_blast.py
@@ -1,10 +1,18 @@
-# SJS 9/6/14.
-# This script processes the Blast output ids. Retains only sequences for which..
-## 1. <1% of the protein sequence is ambiguous, 
-## 2. NCBI annotations does not indicate low-quality or similar
-## 3. A correct, corresponding nucleotide record in NCBI exists.
-## Nucleotide and protein NCBI records for all retained sequences are saved.
-## Output files "protein_records_naive.fasta" and "nucleotide_records_naive.fasta" are saved. Note that these files have the same ids!
+#! /usr/bin/env python
+
+##############################################################################
+## Script to process and filter ids returned by PSI-BLAST and queries the GPCRHMM server to obtain residue domain predictions.
+## We retain only sequences for which:
+##     1. <1% of the protein sequence is ambiguous
+##     2. NCBI annotation does not indicate low-quality, etc.
+##     3. GPCRHMM strongly believes it's a GPCR
+##     4. A correct, corresponding nucleotide record in NCBI exists.
+## Both protein and nucleotide NCBI records are saved, as well as GPCRHMM domain predictions, are saved.
+##
+##
+##
+##  Written by Stephanie J. Spielman (stephanie.spielman@gmail.com).
+##############################################################################
 
 from Bio import SeqIO, Seq, Entrez
 from Bio.Alphabet import generic_dna
@@ -20,6 +28,8 @@ BAD_TAGS = ['LOW_QUALITY', 'PSEUDOGENE', 'PARTIAL', 'LOW QUALITY']
 RESTRICT_TAXA = 'Vertebrata'
 AMBIG_AMINO = ["B", "X", "Z"]
 AMBIG_THRESHOLD = 0.01
+GPCR_GLOBAL = 10
+GPCR_LOCAL = 10
 CDS_MATCH = re.compile("\w\w_\d+\.*\d*")
 
 
@@ -34,7 +44,7 @@ def read_ids(infile):
         id_set.add(line.strip())
     return list(id_set)
    
-
+   
 def fetch_ncbi_record(id, db, ncbi_dir):
     ''' Fetch an NCBI record if not already fetched and saved. '''
     
@@ -50,9 +60,9 @@ def fetch_ncbi_record(id, db, ncbi_dir):
     else:
         record = SeqIO.read(ncbi_dir + id + ".txt", "gb")
     return record
-
- 
-        
+    
+    
+            
 def keep_taxonomy_description(record):
     ''' Return True if taxonomy and description are acceptable, otherwise False. '''
     taxonomy = (" ").join(record.annotations["taxonomy"])
@@ -77,7 +87,7 @@ def keep_ambiguity(sequence):
     else:
         return True
 
-    
+
 def get_CDS_info(prot_record):
     '''Retrieve CDS info (id and location) from a given protein record ''' 
     
@@ -99,9 +109,13 @@ def get_CDS_info(prot_record):
       
     return cds_id, cds_start, cds_end
 
-    
-def retrieve_nuc_record(prot_record, protseq, ncbi_dir):
 
+
+
+    
+def retrieve_nuc_record(prot_record, protseq):
+    ''' Obtain a nucleotide record from NCBI and ensure it "plays nicely" with its corresponding protein record.'''
+    
     # Obtain CDS info and double check it
     cds_id, cds_start, cds_end = get_CDS_info(prot_record)
     cds_length = cds_end - cds_start
@@ -119,8 +133,73 @@ def retrieve_nuc_record(prot_record, protseq, ncbi_dir):
             return None, None, None
     else:
         return None, None, None
-       
 
+
+
+def ensure_gpcr(sequence):
+    ''' Run gpcrhmm to determine if we really have a GPCR here. Use conservative thresholds. ''' 
+    
+    # Create input fasta file for gpcrhmm, and execute.
+    temp = open("in.fasta", "w")
+    temp.write(">id\n"+sequence+"\n")
+    temp.close()
+    run = subprocess.call("perl gpcrhmm/gpcrhmm.pl in.fasta > out.fasta", shell=True)
+    assert(run == 0), "gpcrhmm did not properly run and returned with exit code 0."
+    
+    # Parse the output file
+    outf = open("out.fasta", "rU")
+    line = outf.readlines()[1]
+    outf.close()
+    os.remove("in.fasta")
+    os.remove("out.fasta")
+    get_scores = re.search('^id\s+(-*\d*\.*\d*)\s+(-*\d*\.*\d*)\s+(\w+)\s*', line)
+    assert(get_scores), "Couldn't parse the gpcrhmm output file."
+    global_score = get_scores.group(1)
+    local_score = get_scores.group(2)
+    pred = get_scores.group(3)
+    if pred == 'GPCR' and float(global_score) >= GPCR_GLOBAL and float(local_score) >= GPCR_LOCAL:
+        return True
+    else:
+        return False
+
+
+
+
+    
+def collect_gpcrhmm_structure(protseq, outfile):
+    ''' Query the gpcrhmm server for the structure/domains. '''
+
+    url1 = 'http://gpcrhmm.sbc.su.se/cgi-bin/predict'
+    values = {'protseq':protseq, 'format':'plp'}
+    
+    data = urllib.urlencode(values)
+    req = urllib2.Request(url1, data)
+    no_response1 = True
+    while no_response1:
+        try:    
+            response1 = urllib2.urlopen(req)
+            no_response1 = False
+        except:
+            pass        
+    page1=str(response1.read())
+    find_link=re.search('tmp/(\w\w\w\w)', page1)
+    if find_link:
+        link=str(find_link.group(1))
+        url2='http://gpcrhmm.sbc.su.se/tmp/'+link+'.plp'
+        no_response2 = True
+        while no_response2:
+            try:    
+                response2 = urllib2.urlopen(url2)
+                no_response2 = False
+            except:
+                pass     
+        page2=str(response2.read())
+     
+        outf=open(outfile,'w')
+        outf.write(page2)
+        outf.close()  
+
+               
 
 def main():
   
@@ -128,10 +207,12 @@ def main():
     assert(len(sys.argv) == 3), "Usage: python run_process_blast.py <input_file> <rdir>. Input file should contain list of ncbi protein ids to process. rdir is return directory for output gpcrhmm, ncbi files."
     infile = sys.argv[1]  
     rdir = sys.argv[2]
-    protein_outfile   = "protein_records_naive.fasta"
-    nuc_outfile       = "nucleotide_records_naive.fasta"
+    protein_outfile   = "protein_records.fasta"
+    nuc_outfile       = "nucleotide_records.fasta"
     ncbi_directory    = rdir + "/ncbi_records/"
+    gpcrhmm_directory = rdir + "/gpcrhmm_records/"
     os.mkdir(ncbi_directory)
+    os.mkdir(gpcrhmm_directory)
         
     # Read in ids, while removing duplicates
     print "Reading in ids and removing duplicates"
@@ -139,37 +220,49 @@ def main():
     
     # Loop over ids and decide whether to keep each one or not.
     for prot_id in raw_ids:
-        print "Processing", prot_id
+        
         
         # Retreive the NCBI record
+        print "Processing", prot_id
         prot_record = fetch_ncbi_record(prot_id, "protein", ncbi_directory)
         protseq = str(prot_record.seq)
+        
         
         # Determine if we should keep based on taxonomy, description, and ambiguities
         print "Assessing taxonomy, description, ambiguities"
         if keep_taxonomy_description(prot_record) and keep_ambiguity(protseq):
-                        
-            # Fetch the sequence's corresponding nucleotide record from NCBI, and double-check that it plays nicely with the protein record.
-            print "Fetching nucleotide record and comparing to protein record"
-            nuc_record, nuc_id, nucseq = retrieve_nuc_record(prot_record, protseq, ncbi_directory)
             
-            # If fetching the nucleotide record went ok, we can proceed to save both the protein and the nucleotide NCBI records.
-            if nuc_record is not None:
-                                 
-                # Save the NCBI records
-                SeqIO.write(prot_record, ncbi_directory + prot_id+".txt", "gb")
-                SeqIO.write(nuc_record, ncbi_directory + nuc_id+".txt", "gb")
+            # Ensure that the sequence is really a GPCR. Note that we don't include this in the previous line in order to avoid excessive calls to gpcrhmm
+            print "Checking if GPCR"
+            if ensure_gpcr(protseq):
                 
-                # Save the sequences
-                fullid = prot_id + "_" + nuc_id 
-                with open(protein_outfile, 'a') as prot_handle:
-                    prot_handle.write(">" + fullid + "\n" + protseq + "\n")
-                with open(nuc_outfile, 'a') as nuc_handle:
-                    nuc_handle.write(">" + fullid + "\n" + nucseq + "\n")
+                # Fetch the sequence's corresponding nucleotide record from NCBI, and double-check that it plays nicely with the protein record.
+                print "Fetching nucleotide record and comparing to protein record"
+                nuc_record, nuc_id, nucseq = retrieve_nuc_record(prot_record, protseq, ncbi_directory)
                 
+                # If fetching the nucleotide record went ok, we can proceed to save both the protein and the nucleotide NCBI records.
+                if nuc_record is not None:
+                    
+                    print "We are keeping", prot_id
+                    # Retrieve the gpcrhmm record from their server
+                    collect_gpcrhmm_structure(protseq, gpcrhmm_directory+prot_id+".txt")
+                    assert(os.path.exists(gpcrhmm_directory+prot_id+".txt")), "Didn't get gpcrhmm"
+                     
+                    # Save the NCBI records
+                    SeqIO.write(prot_record, ncbi_directory + prot_id+".txt", "gb")
+                    SeqIO.write(nuc_record, ncbi_directory + nuc_id+".txt", "gb")
+                    
+                    # Save the sequences
+                    fullid = prot_id + "_" + nuc_id 
+                    with open(protein_outfile, 'a') as prot_handle:
+                        prot_handle.write(">" + fullid + "\n" + protseq + "\n")
+                    with open(nuc_outfile, 'a') as nuc_handle:
+                        nuc_handle.write(">" + fullid + "\n" + nucseq + "\n") 
+                
+                else:
+                   print "We are not saving", prot_id, "due to poor nucleotide record."
             else:
-               print "We are not saving", prot_id, "due to poor nucleotide record."
-               
+                print "We are not saving", prot_id, "because it may not be a GPCR."
         else:
             print "We are not saving", prot_id, "due to taxonomy/sequence quality/ambiguities."
             
